79783d70a9090a24280242413b2be48ce6422599
kamgueeclement-cmyk <kamgueeclement@gmail.com>
2026-06-01 17:19:05 +0200

    Create API and JSON Data Reader project

--- project2_api_json.py
+++ project2_api_json.py
@@ -1,1 +1,24 @@
+import urllib.request
+import json
 
+name = input("Enter a first name: ")
+
+url = f"https://api.agify.io/?name={name}"
+
+print("Retrieving:", url)
+
+response = urllib.request.urlopen(url)
+
+data = response.read().decode()
+
+print("\nRaw JSON Data:")
+print(data)
+
+info = json.loads(data)
+
+print("\nProcessed Results")
+print("-" * 30)
+
+print("Name:", info["name"])
+print("Predicted Age:", info["age"])
+print("Records Analyzed:", info["count"])
